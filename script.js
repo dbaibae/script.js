a6e1ea06a2e72de2f24b21bcce0d09f341e65f51
dbaibae <uwakwechekwube165@gmail.com>
2022-06-25 02:45:35 +0100

    my calculator task

---
+++ script.js
@@ -0,0 +1,46 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function modulus(a, b) {
+  return a % b;
+}
+
+function calculate() {
+  var a = Number(prompt("Enter first number"));
+  var b = Number(prompt("Enter second number"));
+  var operator = prompt("Enter operator from the list below: +, -, *, /, %");
+  var result;
+  switch (operator) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = subtract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+    case "%":
+      result = modulus(a, b);
+      break;
+    default:
+      alert("Invalid operator");
+      break;
+  }
+  return alert(result);
+  ;
+}
+calculate();
+
